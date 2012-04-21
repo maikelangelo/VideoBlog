503c59d016f86370f14c6a32a34aee8b4c0e39ef
Miguel Alvarez <miguel.alvarez.t@gmail.com>
2012-04-21 11:10:16 +0200

    new videoblog creado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videoblog::Application.config.secret_token = 'b32b1de8468b53907f8bbe528bb7408dd06708d3f2d458c0af82464e2b097f78b51dc5220b6e06ea854a16380db3c30fc48d4f8d5881d2e573a6814056994cf4'
